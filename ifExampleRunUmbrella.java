3008c9a914e801ac5ccf3e5bb9db4f3cdc460a63
Thanuja-9014 <kurubathanuja15@gmail.com>
2025-06-02 09:54:12 +0530

    Add files via upload

---
+++ ifExampleRunUmbrella.java
@@ -0,0 +1,17 @@
+package balaji;
+
+public class ifExampleRunUmbrella {
+
+	public static void main(String[] args) {
+		boolean isRaining=true;
+		if(isRaining) {
+			System.out.println("Take an umbrella");
+		}
+		else {
+			System.out.println("no need of an umbrella");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
