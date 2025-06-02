342d86d2f075e181253d8327de35b87267d6ca41
Thanuja-9014 <kurubathanuja15@gmail.com>
2025-06-02 10:46:12 +0530

    Add files via upload

---
+++ WrapperExample.java
@@ -0,0 +1,16 @@
+package balaji;
+
+public class WrapperExample {
+
+	public static void main(String[] args) {
+		int a=1;
+		Integer b=1;
+		
+		double pp=99.0;
+		Double ppe=99.0;
+		
+		System.out.println(a);
+		System.out.println(b);
+		// TODO Auto-generated method stub
+	}
+}
