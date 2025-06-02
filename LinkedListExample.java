f06aa462343acd625bf62fbc1b6b130d8019b13b
Thanuja-9014 <kurubathanuja15@gmail.com>
2025-06-02 11:52:31 +0530

    Add files via upload

--- LinkedListExample.java
+++ LinkedListExample.java
@@ -1,4 +1,5 @@
 package balaji;
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class LinkedListExample {
@@ -7,7 +8,27 @@ public class LinkedListExample {
 		LinkedList<String> ob=new LinkedList<>();
 		ob.add("aa");
 		ob.add("bb");
+		ob.add("cc");
+		ob.add("dd");
+		ob.add("zz");
+		ob.add("hh");
 		System.out.println(ob);
+		ob.remove(2);
+		ob.remove("bb");
+		System.out.println(ob);
+		Collections.reverse(ob);
+		System.out.println(ob);
+		Collections.sort(ob);
+		System.out.println(ob);
+		int index=ob.indexOf("zz");
+		System.out.println(index);
+		if(ob.contains("ja")) {
+			System.out.println("aa exixted in the list");
+		}
+		else {
+			System.out.println("Not existed");
+		}
+		
 		// TODO Auto-generated method stub
 
 	}
