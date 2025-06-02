62e07860185d934739a80308eb256e8a608b1098
Thanuja-9014 <kurubathanuja15@gmail.com>
2025-06-02 11:26:40 +0530

    Add files via upload

---
+++ LinkedListExample.java
@@ -0,0 +1,15 @@
+package balaji;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		LinkedList<String> ob=new LinkedList<>();
+		ob.add("aa");
+		ob.add("bb");
+		System.out.println(ob);
+		// TODO Auto-generated method stub
+
+	}
+
+}
