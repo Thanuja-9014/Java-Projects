8c643b7328de558478fbc3fb93e699d69788ad00
Thanuja-9014 <kurubathanuja15@gmail.com>
2025-06-02 11:25:29 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,15 @@
+package balaji;
+import java.util.*;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> ob=new ArrayList<Integer>();
+		ob.add(1);
+		ob.add(2);
+		System.out.println(ob);
+		// TODO Auto-generated method stub
+
+	}
+
+}
