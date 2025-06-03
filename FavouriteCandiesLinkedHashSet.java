2cb2221aba9234a8a7a201a08e11e256aae10829
Thanuja-9014 <kurubathanuja15@gmail.com>
2025-06-03 15:00:25 +0530

    Add files via upload

---
+++ FavouriteCandiesLinkedHashSet.java
@@ -0,0 +1,16 @@
+package balaji;
+import java.util.LinkedHashSet;
+public class FavouriteCandiesLinkedHashSet {
+
+	public static void main(String[] args) {
+		LinkedHashSet<String> ob=new LinkedHashSet<String>();
+		ob.add("chocolates");
+		ob.add("Mango");
+		ob.add("Ice creams");
+		System.out.println(ob);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
