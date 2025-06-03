67bf2a95d8efe900092bb945e992eceb1658819d
Thanuja-9014 <kurubathanuja15@gmail.com>
2025-06-03 12:22:41 +0530

    Add files via upload

---
+++ methodoverloadingexample.java
@@ -0,0 +1,32 @@
+package balaji;
+
+class Animal {
+    void sound() {
+        System.out.println("Animals make sound");
+    }
+}
+
+class Dog extends Animal {
+   
+    void sound() {
+        System.out.println("Dog Barks");
+    }
+}
+
+class Cat extends Animal {
+    
+    void sound() {
+    		System.out.println("Cat Meows");
+    }
+}
+
+public class methodoverloadingexample {
+    public static void main(String[] args) {
+        Animal a1 = new Animal(); 
+        Dog a2 = new Dog();    
+        Cat a3 = new Cat();    
+        a1.sound(); 
+        a2.sound(); 
+        a3.sound(); 
+    }
+}
